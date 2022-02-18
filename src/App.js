67951058b99d0e9dd46844a83eb9d92f41141afb
codeclan student <codeclanstudent@codeclans-mbp.home>
2022-02-18 13:05:20 +0000

    react environment setup and task completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';    
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+function App() {              
+  // we can write JS here :)
+  const name = "Adam";
+  const age = "32";
+
+  return (                   //JSX = Javascript XML
+     <div><p>My name is {name} and I'm {age} years old.</p>
+     <p>Test</p>
+     <img src="https://thiscatdoesnotexist.com/" alt="random picture of cat"></img>
+     </div>
+  );                          
+}  
 
 export default App;
